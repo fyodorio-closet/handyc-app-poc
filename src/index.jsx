9dccdec5259331ecd68743fae25c178eb969affe
fyodorio <alinetofyodor@gmail.com>
2022-02-14 00:32:42 +0500

    chore(): add bootstrap icons

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'solid-js/web';
 import { Router } from 'solid-app-router';
 
 import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 import App from './App';
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <section>
+    <section class="d-flex align-items-center">
+      <i class="bi bi-house-door-fill"></i>
       <h1>Home</h1>
     </section>
   );
