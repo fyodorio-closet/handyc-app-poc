6d36fd0cef9a6796acadc3fa0ff44e592890a4c0
fyodorio <alinetofyodor@gmail.com>
2022-02-20 09:01:55 +0500

    feat(): update about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,6 +7,7 @@ export default function About() {
         <li>Move hours and routes and other hardcoded stuff out</li>
         <li>Model calendar data and choose tools for dates management</li>
         <li>Stick with common file/code naming conventions (get to know them for Solid/React)</li>
+        <li>Move texts to config</li>
       </ul>
     </section>
   );
